7bb97bad2a2bd4ad7dff0508c6c2f9e6c2266021
sicktheme <housed123@mail.ru>
2023-07-20 16:05:33 +0700

    Adding a platform Adding a balloon Adding highlight to the ball

--- Popcorn/Engine.cpp
+++ Popcorn/Engine.cpp
@@ -1,7 +1,7 @@
 #include "Engine.h"
 
-HPEN Brick_Red_Pen, Brick_Blue_Pen;
-HBRUSH Brick_Red_Brush, Brick_Blue_Brush;
+HPEN Highlight_Pen, Brick_Red_Pen, Brick_Blue_Pen, Platform_Circle_Pen, Platform_Inner_Pen;
+HBRUSH Brick_Red_Brush, Brick_Blue_Brush, Platform_Circle_Brush, Platform_Inner_Brush;
 
 const int Global_Scale = 3;
 const int Brick_Width = 15;
@@ -10,6 +10,9 @@ const int Cell_Width = 16;
 const int Cell_Height = 8;
 const int Level_X_Offset = 8;
 const int Level_Y_Offset = 6;
+const int Circle_Size = 7;
+
+int Inner_Width = 21;
 
 enum EBrick_Type {
     EBT_None,
@@ -34,14 +37,22 @@ char Level_01[14][12] = {
     0,0,0,0,0,0,0,0,0,0,0,0
 };
 
+void Create_Pen_Brush(unsigned char r, unsigned char g, unsigned char b, HPEN& pen, HBRUSH& brush) {
+    // Создание кисти и карандаша 
+
+    pen = CreatePen(PS_SOLID, 0, RGB(r, g, b));
+    brush = CreateSolidBrush(RGB(r, g, b));
+}
+
 void Init() {
     // Инициализация
 
-    Brick_Blue_Pen = CreatePen(PS_SOLID, 0, RGB(85, 255, 255));
-    Brick_Blue_Brush = CreateSolidBrush(RGB(85, 255, 255));
+    Highlight_Pen = CreatePen(PS_SOLID, 0, RGB(255, 255, 255));
 
-    Brick_Red_Pen = CreatePen(PS_SOLID, 0, RGB(255, 85, 85));
-    Brick_Red_Brush = CreateSolidBrush(RGB(255, 85, 85));
+    Create_Pen_Brush(85, 255, 255, Brick_Blue_Pen, Brick_Blue_Brush);
+    Create_Pen_Brush(255, 85, 85, Brick_Red_Pen, Brick_Red_Brush);
+    Create_Pen_Brush(7,254,118,Platform_Circle_Pen, Platform_Circle_Brush);
+    Create_Pen_Brush(241,254,197, Platform_Inner_Pen, Platform_Inner_Brush);
 }
 
 void Draw_Brick(HDC hdc, int x, int y, EBrick_Type is_blue) {
@@ -72,7 +83,6 @@ void Draw_Brick(HDC hdc, int x, int y, EBrick_Type is_blue) {
     SelectObject(hdc, brush);
 
     RoundRect(hdc, x * Global_Scale, y * Global_Scale, (x + Brick_Width) * Global_Scale, (y + Brick_Height) * Global_Scale, 2 * Global_Scale, 2 * Global_Scale);
-
 }
 
 void Draw_Level(HDC hdc) {
@@ -83,8 +93,36 @@ void Draw_Level(HDC hdc) {
             Draw_Brick(hdc, Level_X_Offset + j * Cell_Width, Level_Y_Offset + i * Cell_Height, (EBrick_Type)Level_01[i][j]);
 }
 
+void Draw_Platform(HDC hdc, int x,int y) {
+    // Отрисовка платформы
+
+    // 1. Отрисовка боковых шариков
+    SelectObject(hdc, Platform_Circle_Pen);
+    SelectObject(hdc, Platform_Circle_Brush);
+
+    Ellipse(hdc, x * Global_Scale, y * Global_Scale, (x + Circle_Size) * Global_Scale, (y + Circle_Size) * Global_Scale);
+    Ellipse(hdc, (x + Inner_Width) * Global_Scale, y * Global_Scale, (x + Circle_Size + Inner_Width) * Global_Scale,
+        (y + Circle_Size) * Global_Scale);
+
+    // 2. Отрисовка блика
+    SelectObject(hdc, Highlight_Pen);
+
+    Arc(hdc, (x + 1) * Global_Scale, (y + 1) * Global_Scale, (x + Circle_Size - 1) * Global_Scale,
+        (y + Circle_Size - 1) * Global_Scale, (x + 1 + 1) * Global_Scale, (y + 1) * Global_Scale,
+        (x + 1) * Global_Scale, (y + 2) * Global_Scale);
+
+    // 3. Отрисовка средней части
+    SelectObject(hdc, Platform_Inner_Pen);
+    SelectObject(hdc, Platform_Inner_Brush);
+
+    RoundRect(hdc, (x + 4) * Global_Scale, (y + 1) * Global_Scale, (x + 4 + Inner_Width - 1) * Global_Scale, (y + 1 + 5) * Global_Scale,
+        3 * Global_Scale, 3 * Global_Scale);
+}
+
 void Draw_Frame(HDC hdc) {
     // Отрисовка экрана
 
     Draw_Level(hdc);
+
+    Draw_Platform(hdc, 50, 100);
 }
